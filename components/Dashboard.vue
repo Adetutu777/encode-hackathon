91802ab354e5f12bf6708e3b54a6a40ffa07b2e8
oke kehinde <okekehinde@gmail.com>
2023-01-07 21:36:10 +0100

    misc

--- components/Dashboard.vue
+++ components/Dashboard.vue
@@ -1,84 +1,83 @@
 <template>
-  <NuxtLayout name="sidebar">
-    <div>
-      <div
-        v-if="isPending"
-        class="d-flex justify-content-center w-full p-2 my-0 py-3 mx-auto top-banner"
-      >
-        <div class="glass">
-          <topbanner />
-        </div>
-        <div class="">Profile Creation Pending</div>
+  <div>
+    <div
+      v-if="isPending"
+      class="d-flex justify-content-center w-full p-2 my-0 py-3 mx-auto top-banner"
+    >
+      <div class="glass">
+        <topbanner />
       </div>
+      <div class="">Profile Creation Pending</div>
+    </div>
 
-      <div class="container">
-        <div class="">
-          <profileId />
-        </div>
-        <div class="row mt-3">
-          <div class="col-md-3">
-            <div class="">
-              <div class="card-ody">
-                <div class="sidebar sidebar-left">
-                  <div class="sidebar-inner">
-                    <NuxtLink to="/blogs" class="dropdown d-block">
-                      <i class="icon-icons uil uil-estate"></i>
-                      <span>Home</span>
-                    </NuxtLink>
-                    <!-- <NuxtLink to="/help" class="dropdown d-block">
+    <div class="container">
+      <div class="">
+        <profileId />
+      </div>
+      <div class="row mt-3">
+        <div class="col-md-3">
+          <div class="">
+            <div class="card-ody">
+              <div class="sidebar">
+                <div class="sidebar-inner">
+                  <NuxtLink to="/blogs" class="dropdown d-block">
+                    <i class="icon-icons uil uil-estate sidebar-left"></i>
+                    <span>Home</span>
+                  </NuxtLink>
+                  <!-- <NuxtLink to="/help" class="dropdown d-block">
                     <i class="icon-icons uil uil-envelope-add"></i>
                     <span>Notifications</span>
                   </NuxtLink> -->
-                    <!-- <NuxtLink to="/userSetting" class="dropdown d-block">
+                  <!-- <NuxtLink to="/userSetting" class="dropdown d-block">
                     <i class="icon-icons uil uil-setting"></i>
                     <span>Settings</span>
                   </NuxtLink> -->
-                    <NuxtLink
-                      :to="`/profile/${defaultProfile?.ownedBy}`"
-                      class="dropdown d-block"
-                    >
-                      <i class="icon-icons uil uil-user-circle"></i>
-                      <span>Profile</span>
-                    </NuxtLink>
-                    <NuxtLink to="/draft" class="dropdown d-block">
-                      <i class="icon-icons uil uil-question-circle"></i>
-                      <span>Draft</span>
-                    </NuxtLink>
-                    <NuxtLink
-                      to="/post/createstory"
-                      class="write-dropdown d-block text-center text-light create-post mt-5"
-                    >
-                      <span class="">Create post</span>
-                    </NuxtLink>
-                  </div>
+                  <NuxtLink
+                    :to="`/profile/${defaultProfile?.ownedBy}`"
+                    class="dropdown d-block"
+                  >
+                    <i class="icon-icons uil uil-user-circle sidebar-left"></i>
+                    <span>Profile</span>
+                  </NuxtLink>
+                  <NuxtLink to="/draft" class="dropdown d-block">
+                    <i
+                      class="icon-icons uil uil-question-circle sidebar-left"
+                    ></i>
+                    <span>Draft</span>
+                  </NuxtLink>
+                  <NuxtLink
+                    to="/post/createstory"
+                    class="write-dropdown d-block text-center text-light create-post mt-5"
+                  >
+                    <span class="">Create post</span>
+                  </NuxtLink>
                 </div>
               </div>
             </div>
           </div>
-          <div class="col-md-6">
-            <div class="">
-              <div class="card-body">
-                <div cass=" p-4">
-                  <slot name="middle"></slot>
-                </div>
+        </div>
+        <div class="col-md-6">
+          <div class="">
+            <div class="card-body">
+              <div cass=" p-4">
+                <slot name="middle"></slot>
               </div>
             </div>
           </div>
-          <div class="col-md-3">
-            <div class="">
-              <div class="card-ody">
-                <div class="right-bar p-4">
-                  <slot name="right"></slot>
-                </div>
+        </div>
+        <div class="col-md-3">
+          <div class="">
+            <div class="card-ody">
+              <div class="right-bar p-4" v-if="$slots.right">
+                <slot name="right"></slot>
               </div>
             </div>
           </div>
         </div>
       </div>
     </div>
-  </NuxtLayout>
+  </div>
 </template>
-
 <script>
 import { login } from "../services/connect";
 import { useAppStore } from "../store/app";
@@ -96,7 +95,6 @@ export default {
         login();
       }
     });
-
     return { defaultProfile, isPending };
   },
 };
@@ -111,12 +109,11 @@ export default {
 .sidebar,
 .right-bar {
   padding-bottom: 4rem;
-  backgrond: #f9fbff;
-  background: #fffcfc;
-  border: 1px solid #ececec;
+  background: F9FBFF;
+  background: #ffffff;
+  border: 1.5px solid #ececec;
   border-radius: 16px;
 }
-
 .middle-bar {
   border-radius: 7px;
   margin: 0 1rem;
@@ -126,7 +123,6 @@ export default {
   background: #ffffff;
   border-radius: 12px;
 }
-
 .icon-icons {
   font-size: 23px;
   vertical-align: middle;
@@ -141,7 +137,6 @@ export default {
   padding: 1rem 0;
   text-decoration: none;
 }
-
 .write-dropdown:hover {
   background: #66a7df;
 }
@@ -149,14 +144,12 @@ a.dropdown:hover {
   background: #f2f9ff;
   border-radius: 15px;
 }
-
 .router-link-active,
 .router-link-active i,
 .router-link-active span {
   color: #2c74b3 !important;
   font-weight: bold;
 }
-
 .card-image {
   max-width: 50rem;
 }
@@ -167,13 +160,11 @@ a.dropdown:hover {
 .icon-footer {
   font-size: 1.8rem;
 }
-
 .trending {
   background: #f4f8ff;
   border-radius: 10000px;
   padding: 0.5rem;
 }
-
 .top-header {
   padding: 1rem;
   background: #ffffff;
@@ -181,7 +172,6 @@ a.dropdown:hover {
   border-radius: 16px;
   transform: translateY(-1rem);
 }
-
 .wrapper {
   background: #f9fbff;
 }
@@ -192,7 +182,6 @@ a.dropdown:hover {
   padding: 1rem;
   margin-bottom: 1rem;
 }
-
 .img-icon {
   margin-right: 1rem;
 }
@@ -200,7 +189,6 @@ a.dropdown:hover {
   background: #ffffff !important;
   position: sticky !important;
 }
-
 .img-top {
   width: 2rem;
   height: 2rem;
@@ -211,17 +199,15 @@ a.dropdown:hover {
   height: 1.3rem;
   margin-right: 0.5rem;
 }
-
 .middle-info {
   border: 1px solid red;
   margin-bottom: 5rem;
 }
-
 .create-post {
   background: #2c74b3;
   border-radius: 2rem;
+  margin: 0 2.5rem;
 }
-
 .top-banner {
   font-weight: 700;
   background: #66a7df;
@@ -229,5 +215,8 @@ a.dropdown:hover {
 }
 .glass {
   margin-right: 0.8rem;
+}
+.sidebar-left {
+  padding-left: 1rem;
 }
 </style>
--- pages/index.vue
+++ pages/index.vue
@@ -1,203 +1,213 @@
 <template>
-  <NuxtLayout name="sidebar">
-    <div>
-      <!-- navbar -->
-      <div class="lens-story">
-        <div class="nav-header px-4 py-4 d-flex justify-content-between">
-          <div>
-            <img src="@/images/Chain-write.svg" alt="image" />
-          </div>
-
-          <div class="">
-            <CreateAccountWallet classes="btn-signup mt-2" />
-          </div>
+  <div>
+    <!-- navbar -->
+    <div class="lens-story">
+      <div class="nav-header px-4 py-4 d-flex justify-content-between">
+        <div>
+          <img class="w-75" src="@/images/Chain-write.svg" alt="image" />
         </div>
-        <!-- navbar ends here -->
 
-        <!-- body -->
-        <div class="container">
-          <div class="row algn-items-start">
-            <div class="col-md-1"></div>
-            <div class="col-md-5 col-one">
-              <h2>The All-in-One Web3 Blogging Platform!</h2>
-              <p class="mt-4">
-                Experience a New Social Media Revolution with chainWrite, This
-                is a perfect platform for anyone looking to express themselves
-                online without worrying about their data being shared or
-                exploited
-              </p>
-
-              <div class="d-flex mt-5">
-                <button class="btn-one"><b>Get started</b></button>
-              </div>
-            </div>
-            <!-- col-two -->
-            <div class="col-md-6 mt-5">
-              <img
-                class="bg-image img-fluid"
-                src="@/images/chainwritesmile.svg"
-                alt="image"
-              />
-            </div>
-          </div>
-
-          <!-- footer -->
-
-          <!-- End of .container -->
+        <div class="">
+          <CreateAccountWallet classes="btn-signup mt-2" />
         </div>
       </div>
+      <!-- navbar ends here -->
 
-      <!-- about-us -->
-      <div class="about-us">
-        <div class="container my-5">
-          <div class="row">
-            <div class="col-md-6 text-center">
-              <img
-                src="https://images.pexels.com/photos/3184428/pexels-photo-3184428.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500"
-                class="img-fluid about-img"
-                style="box-shadow: 0 0 15px #eee"
-                alt=""
-              />
+      <!-- body -->
+      <div class="container">
+        <div class="row algn-items-start">
+          <!-- <div class="col-md-1"></div> -->
+          <div class="col-md-6 col-one">
+            <h2 class="head-text">
+              The All-in-One Web3 Blogging <br />
+              Platform!
+            </h2>
+            <p class="mt-4 hero-text">
+              Experience a New Social Media Revolution with chainWrite, This is
+              a perfect <br />
+              platform for anyone looking to express themselves online without
+              worrying about <br />
+              their data being shared or exploited
+            </p>
+
+            <div class="d-flex mt-4">
+              <button class="btn-one"><b>Get started</b></button>
             </div>
-            <div class="col-md-6 pt-3">
-              <small class="text-uppercase about-heading" style="color: #66a7df"
-                >About us</small
+          </div>
+          <!-- col-two -->
+          <div class="col-md-6 mt-5">
+            <img
+              class="bg-image img-fluid"
+              src="@/images/chainwritesmile.svg"
+              alt="image"
+            />
+          </div>
+        </div>
+
+        <!-- footer -->
+
+        <!-- End of .container -->
+      </div>
+    </div>
+
+    <!-- about-us -->
+    <div class="about-us">
+      <div class="container my-5">
+        <div class="row">
+          <div class="col-md-6 text-center">
+            <img
+              src="https://images.pexels.com/photos/3184428/pexels-photo-3184428.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500"
+              class="img-fluid about-img"
+              style="box-shadow: 0 0 15px #eee"
+              alt=""
+            />
+          </div>
+          <div class="col-md-6 pt-4">
+            <small class="text-uppercase about-heading" style="color: #66a7df"
+              >About us</small
+            >
+            <h1 class="h2 mb-4" style="font-weight: 600">
+              About Our
+              <span style="color: #66a7df" class="font-weight-bold"
+                >Company</span
               >
-              <h1 class="h2 mb-4" style="font-weight: 600">
-                About Our
-                <span style="color: #66a7df" class="font-weight-bold"
-                  >Company</span
-                >
-              </h1>
-              <p class="about-text">
-                ChainWrite was founded in 2023 by a team of experienced
-                developers and entrepreneurs who wanted to make a difference. We
-                are committed to developing a social media platform that is
-                secure and private, while also being easy to use. We want our
-                users to feel confident that their data is not being shared or
-                sold, or exploited in any way.
-              </p>
-            </div>
-          </div>
-        </div>
-      </div>
-
-      <div class="meet-team bg-light">
-        <div class="d-team">
-          <div class="container">
-            <div class="row heading">
-              <h2 class="text-center bottom-line pt-5">Meet Our Team</h2>
-              <p class="subheading text-center pb-3">Creative Nerds</p>
-            </div>
-
-            <div class="row team-row">
-              <div class="col-md-3"></div>
-              <div class="col-md-3 col-sm-6 team-wrap">
-                <div class="team-member text-center">
-                  <div class="team-img">
-                    <img src="@/images/ken.png" alt="" />
-                    <div class="overlay">
-                      <div class="team-details text-center">
-                        <p>Creating software with humans in mind</p>
-                        <div class="socials mt-20">
-                          <a href="#"><i class="fa fa-facebook"></i></a>
-                          <a href="#"><i class="fa fa-twitter"></i></a>
-                          <a href="#"><i class="fa fa-google-plus"></i></a>
-                          <a href="#"><i class="fa fa-envelope"></i></a>
-                        </div>
-                      </div>
-                    </div>
-                  </div>
-                  <h6 class="team-title">Oke Kehinde</h6>
-                  <span>FullStack Software developer</span>
-                </div>
-              </div>
-              <!-- end team member -->
-
-              <div class="col-md-3 col-sm-6 team-wrap">
-                <div class="team-member text-center">
-                  <div class="team-img">
-                    <img src="@/images/tut.jpg" alt="" />
-                    <div class="overlay">
-                      <div class="team-details text-center">
-                        <p>Bringing your digital dreams to life</p>
-                        <div class="socials mt-20">
-                          <a href="#"><i class="fa fa-facebook"></i></a>
-                          <a href="#"><i class="fa fa-twitter"></i></a>
-                          <a href="#"><i class="fa fa-google-plus"></i></a>
-                          <a href="#"><i class="fa fa-envelope"></i></a>
-                        </div>
-                      </div>
-                    </div>
-                  </div>
-                  <h6 class="team-title">Adetutu Gbangbola</h6>
-                  <span>Software developer</span>
-                </div>
-              </div>
-              <!-- end team member -->
-
-              <div class="col-md-3 col-sm-6 team-wrap"></div>
-              <!-- end team member -->
-            </div>
-          </div>
-        </div>
-      </div>
-
-      <!-- footer -->
-      <div class="pt-5 pb-3 chain-footer footer">
-        <div class="container">
-          <div class="row">
-            <div class="col-lg-5 col-xs-12 about-company">
-              <img src="@/images/Chain-write.svg" alt="image" />
-              <p class="pr-5 text-light mt-3">
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam
-                ac ante mollis quam tristique convallis
-              </p>
-              <p>
-                <a href="#"><i class="fa fa-facebook-square mr-1"></i></a
-                ><a href="#"><i class="fa fa-linkedin-square"></i></a>
-              </p>
-            </div>
-            <div class="col-lg-3 col-xs-12 links">
-              <h4 class="mt-lg-0 mt-sm-3">Links</h4>
-              <ul class="m-0 p-0">
-                <li>- <a href="#">Lorem ipsum</a></li>
-                <li>- <a href="#">Nam mauris velit</a></li>
-                <li>- <a href="#">Etiam vitae mauris</a></li>
-                <li>- <a href="#">Fusce scelerisque</a></li>
-                <li>- <a href="#">Sed faucibus</a></li>
-                <li>- <a href="#">Mauris efficitur nulla</a></li>
-              </ul>
-            </div>
-            <div class="col-lg-4 col-xs-12 location">
-              <h4 class="mt-lg-0 mt-sm-4">Location</h4>
-              <p>22, Lorem ipsum dolor, consectetur adipiscing</p>
-              <p class="mb-0">
-                <i class="fa fa-phone mr-3"></i>(+234) 0000 0000
-              </p>
-              <p><i class="fa fa-envelope-o mr-3"></i>info@chainWrite.com</p>
-            </div>
-          </div>
-          <div class="row mt-5">
-            <div class="col copyright">
-              <p class="">
-                <small class="text-white-50"
-                  >© 2023. All Rights Reserved.</small
-                >
-              </p>
-            </div>
+            </h1>
+            <p class="about-text">
+              ChainWrite was founded in 2023 by a team of experienced developers
+              and entrepreneurs who wanted to make a difference. We are
+              committed to developing a social media platform that is secure and
+              private, while also being easy to use. We want our users to feel
+              confident that their data is not being shared or sold, or
+              exploited in any way.
+            </p>
           </div>
         </div>
       </div>
     </div>
-  </NuxtLayout>
+
+    <div class="meet-team bg-light">
+      <div class="d-team">
+        <div class="container">
+          <div class="row heading">
+            <h2 class="text-center bottom-line pt-5">Meet Our Team</h2>
+            <p class="subheading text-center pb-3">Creative Nerds</p>
+          </div>
+
+          <div class="row team-row">
+            <div class="col-md-3"></div>
+            <div class="col-md-3 col-sm-6 team-wrap">
+              <div class="team-member text-center">
+                <div class="team-img">
+                  <img src="@/images/ken.png" alt="" />
+                  <div class="overlay">
+                    <div class="team-details text-center">
+                      <p>Creating software with humans in mind</p>
+                      <div class="socials mt-20">
+                        <a href="https://twitter.com/okekehi"
+                          ><i class="uil uil-twitter"></i
+                        ></a>
+                        <a href="https://github.com/okeken"
+                          ><i class="uil uil-github"></i
+                        ></a>
+                        <!-- <a href="okekehinde@gmail.com"><i class="uil uil-envelope-add"></i></a> -->
+                        <a href="https://www.linkedin.com/in/okeken/"
+                          ><i class="uil uil-linkedin"></i
+                        ></a>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+                <h6 class="team-title">Oke Kehinde</h6>
+                <span>FullStack Software developer</span>
+              </div>
+            </div>
+            <!-- end team member -->
+
+            <div class="col-md-3 col-sm-6 team-wrap">
+              <div class="team-member text-center">
+                <div class="team-img">
+                  <img src="@/images/tut.jpg" alt="" />
+                  <div class="overlay">
+                    <div class="team-details text-center">
+                      <p>Bringing your digital dreams to life</p>
+                      <div class="socials mt-20">
+                        <a href="https://twitter.com/adetutu2222"
+                          ><i class="uil uil-twitter"></i
+                        ></a>
+                        <a href="https://github.com/Adetutu777"
+                          ><i class="uil uil-github"></i
+                        ></a>
+                        <!-- <a href="adetutugbangbola@gmail.com"><i class="uil uil-envelope-add"></i></a> -->
+                        <a
+                          href="https://www.linkedin.com/in/adetutu-gbangbola-05a48070/"
+                          ><i class="uil uil-linkedin"></i
+                        ></a>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+                <h6 class="team-title">Adetutu Gbangbola</h6>
+                <span>Software developer</span>
+              </div>
+            </div>
+            <!-- end team member -->
+
+            <div class="col-md-3 col-sm-6 team-wrap"></div>
+            <!-- end team member -->
+          </div>
+        </div>
+      </div>
+    </div>
+
+    <!-- footer -->
+    <div class="pt-5 pb-3 chain-footer footer">
+      <div class="container">
+        <div class="row">
+          <div class="col-lg-5 col-xs-12 about-company">
+            <img src="@/images/Chain-write.svg" alt="image" />
+            <p class="pr-5 text-light mt-3">
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam ac
+              ante mollis quam tristique convallis
+            </p>
+            <p>
+              <a href="#"><i class="fa fa-facebook-square mr-1"></i></a
+              ><a href="#"><i class="fa fa-linkedin-square"></i></a>
+            </p>
+          </div>
+          <div class="col-lg-3 col-xs-12 links">
+            <h4 class="mt-lg-0 mt-sm-3">Links</h4>
+            <ul class="m-0 p-0">
+              <li>- <a href="#">Lorem ipsum</a></li>
+              <li>- <a href="#">Nam mauris velit</a></li>
+              <li>- <a href="#">Etiam vitae mauris</a></li>
+              <li>- <a href="#">Fusce scelerisque</a></li>
+              <li>- <a href="#">Sed faucibus</a></li>
+              <li>- <a href="#">Mauris efficitur nulla</a></li>
+            </ul>
+          </div>
+          <div class="col-lg-4 col-xs-12 location">
+            <h4 class="mt-lg-0 mt-sm-4">Location</h4>
+            <p>22, Lorem ipsum dolor, consectetur adipiscing</p>
+            <p class="mb-0"><i class="fa fa-phone mr-3"></i>(+234) 0000 0000</p>
+            <p><i class="fa fa-envelope-o mr-3"></i>info@chainWrite.com</p>
+          </div>
+        </div>
+        <div class="row mt-5">
+          <div class="col copyright">
+            <p class="">
+              <small class="text-white-50">© 2023. All Rights Reserved.</small>
+            </p>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
 </template>
 
 <script>
 import { clientId, createProfile } from "../api";
 import { ethers } from "ethers";
 import { useAppStore } from "../store/app";
-
 export default {
   layout: "no-sidebar",
   setup(props) {
@@ -206,9 +216,7 @@ export default {
     const address = ref("");
     const token = ref("");
     const toggle = ref("");
-
     const appStore = useAppStore();
-
     const signData = async () => {
       const signerOrProvider = new ethers.providers.Web3Provider(
         window?.ethereum
@@ -219,13 +227,11 @@ export default {
       let signature = await signer.signMessage(text);
       let addressy = ethers.utils.verifyMessage(text, signature);
     };
-
     const createAccount = async () => {
       try {
         if (!token.value) {
           await login();
         }
-
         const createUserAcct = await clientId.request(
           createProfile,
           {
@@ -255,7 +261,6 @@ export default {
   },
 };
 </script>
-
 <style>
 .lens-stor {
   background: #1f2833;
@@ -266,21 +271,17 @@ export default {
   background: linear-gradient(160.1deg, #2471b4 0.01%, #65b7ff 95.54%);
   height: 100vh;
 }
-
 .nav-header {
   max-width: 80rem;
   margin: auto;
 }
-
 .your-story {
   color: #66fcf1;
 }
-
 .col-one {
   margin-top: 7rem;
   color: #ffffff;
 }
-
 .bg-image {
   border-radius: 2rem;
 }
@@ -306,27 +307,21 @@ export default {
 .about-chain {
   padding: 2rem 0 3rem 0;
 }
-
 .about-text {
   line-height: 1.8;
 }
-
 /* Our Team
 -------------------------------------------------------*/
-
 .our-team .team-row {
   margin-left: -40px;
   margin-right: -40px;
 }
-
 .our-team .team-wrap {
   padding: 0 40px;
 }
-
 .our-team .container-fluid {
   padding: 0 50px;
 }
-
 .team-img img {
   -webkit-transition: all 0.2s ease-in-out;
   -moz-transition: all 0.2s ease-in-out;
@@ -335,17 +330,14 @@ export default {
   overflow: hidden;
   width: 100%;
 }
-
 .team-member,
 .team-img {
   position: relative;
   overflow: hidden;
 }
-
 .team-title {
   margin: 30px 0 7px;
 }
-
 .overlay {
   background-color: rgba(20, 20, 20, 0.7);
   position: absolute;
@@ -358,7 +350,6 @@ export default {
   -o-transition: all 0.2s ease-in-out;
   transition: all 0.2s ease-in-out;
 }
-
 .team-details {
   opacity: 0;
   position: absolute;
@@ -373,32 +364,26 @@ export default {
   -o-transition: all 0.2s ease-in-out;
   transition: all 0.2s ease-in-out;
 }
-
 .team-details p {
   color: #fff;
 }
-
 .team-img:hover .team-details {
   opacity: 1;
   margin-top: -80px;
 }
-
 .team-img:hover .overlay {
   opacity: 1;
 }
-
 .socials a {
   display: inline-block;
   width: 37px;
   height: 37px;
   background-color: transparent;
-  border: 1px solid red;
 }
-
 .socials i {
   line-height: 37px;
   color: #616161;
-  font-size: 14px;
+  font-size: 1.5rem;
   width: 37px;
   height: 37px;
   border-radius: 50%;
@@ -409,14 +394,11 @@ export default {
   -ms-transition: all 0.2s linear;
   transition: all 0.2s linear;
 }
-
 .team-details .socials i {
   color: #fff;
 }
-
 .socials a:hover i {
-  color: #fff;
-  background-color: #355c7d;
+  color: #66a7df;
 }
 .footer {
   background: #2471b4;
@@ -449,12 +431,15 @@ export default {
 .footer .copyright p {
   border-top: 1px solid rgba(255, 255, 255, 0.1);
 }
-
 .about-img {
   border-radius: 1rem;
 }
-
 .d-team {
   padding: 2rem 0 5rem 0;
+}
+.hero-text,
+.head-text {
+  line-height: 1.5;
+  margin-left: 1.6rem;
 }
 </style>
